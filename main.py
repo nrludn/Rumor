06be04abacc0e7cc9a4340ad8aa7976e2fc708cf
Dursun NURLU <dursun@192.168.1.34>
2024-03-11 21:07:20 +0300

    cleaning

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.get_announcement import get_announce
+from src.get_statement import get_statement
+
+def main():
+    get_announce()
+    get_statement()
+
+if __name__ == "__main__":
+    main()
